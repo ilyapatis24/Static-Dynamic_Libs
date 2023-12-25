f0db6b51c797d74654bc3591f88cb7be6d2083e7
ilyapatis24 <ilyapatis24@mail.ru>
2023-12-26 00:43:39 +0300

    fixing encoding

--- 01/GreeterClient/GreeterClient.cpp
+++ 01/GreeterClient/GreeterClient.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include "greeter.h"
 
 int main(int argc, char** argv) {
 	system("chcp 1251");
 	Greeter greeter;
 	std::string name = "";
-	std::cout << "Ââåäèòå èìÿ: ";
+	std::cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ð¸Ð¼Ñ: ";
 	std::cin >> name;
 	std::cout << greeter.greet(name);
 	return 0;
