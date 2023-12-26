c9e83d045fa474356144e60a6f2ce045adbfc09a
ilyapatis24 <ilyapatis24@mail.ru>
2023-12-26 20:12:45 +0300

    CMake dynamic libraries

---
+++ 03/LeaverClient.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "leaver.h"
+
+int main(int argc, char** argv) {
+	system("chcp 1251");
+	Leaver leaver;
+	std::string name = "";
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << leaver.leave(name);
+	return 0;
+}
---
+++ 03/leaver.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <string>
+
+class Leaver {
+public:
+	Leaver() = default;
+
+	std::string leave(std::string name);
+};
